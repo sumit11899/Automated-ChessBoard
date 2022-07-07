45a2b7f2bab76f7b9b1506f9be688c44f6269588
KarthikIlla <103555571+KarthikIlla@users.noreply.github.com>
2022-07-07 13:27:34 +0530

    Add files via upload

---
+++ Level 2/Micro_Max.h
@@ -0,0 +1,12 @@
+//************************************  PROTOTYPE
+unsigned short myrand(void);
+void gameOver();
+void bkp();
+void serialBoard();
+void AI_player_H();
+void AI_player_C();
+
+//*****************************  EXTERN VARIABLES
+extern char mov [];
+extern byte sequence;
+extern boolean no_valid_move;
